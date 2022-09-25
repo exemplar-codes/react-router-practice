81ef133207cddafbd7b6cc2491b195fbd85d46bb
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-25 18:41:16 +0530

    Add quotes list

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,3 +1,15 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great" },
+];
+
 export default function AllQuotes() {
-  return <h1>All Quotes Page</h1>;
+  return (
+    <>
+      <h1>All Quotes Page</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </>
+  );
 }
