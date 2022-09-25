83f0fe6bb6a8e5ace266ae32ea188e48de6c8e62
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-25 19:06:01 +0530

    Add quote detail

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,13 +1,20 @@
 import { Route, useParams } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
 import Comments from "../components/comments/Comments";
+
+import { DUMMY_QUOTES } from "../App";
 
 export default function QuoteDetail() {
   const { quoteId } = useParams();
 
+  const quote = DUMMY_QUOTES.find(({ id }) => id === quoteId);
+
   return (
     <>
       <h1>Quote Detail Page</h1>
-      <p>Quote ID: {quoteId}</p>
+      {quote ? <HighlightedQuote {...quote} /> : <p>No quote found</p>}
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
