ff75c9e51cbe4f8755649ac9248e720551b85a0a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-25 17:37:16 +0530

    Add 3 needed routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/new">
+        <NewQuote />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+    </Switch>
   );
 }
+
+// Pages
+// 1. All quotes, /quotes
+// 2. Quote detail page, /quotes/:id
+// 3. New quote form, /quotes/new
 
 export default App;
---
+++ src/pages/QuoteDetail.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+
+export default function QuoteDetail() {
+  console.log(useParams());
+  return <h1>Quote Detail</h1>;
+}
