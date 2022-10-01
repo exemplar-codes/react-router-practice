c88e86078d36195124a249cd3ddf895d3c5cf852
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-02 02:41:55 +0530

    Using React Router instead of managing routing state, for some cases

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,4 +1,4 @@
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, Switch, useParams } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -15,6 +15,16 @@ export default function QuoteDetail() {
     <>
       <h1>Quote Detail Page</h1>
       {quote ? <HighlightedQuote {...quote} /> : <p>No quote found</p>}
+      <Route path={`/quotes/${quoteId}`} exact>
+        {quote && (
+          <Link
+            to={`/quotes/${quoteId}/comments`}
+            className="btn--flat centered"
+          >
+            Load comments
+          </Link>
+        )}
+      </Route>
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
