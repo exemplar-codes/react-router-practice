f332aa06e1b4cda187683ba09cab4b85e5b46f15
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-02 17:41:52 +0530

    Create Firebase DB and add link here

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-router-http-53ba1-default-rtdb.asia-southeast1.firebasedatabase.app';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
