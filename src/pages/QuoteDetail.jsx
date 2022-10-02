49043e8adab007c831b986a792a83c00e5653524
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-02 18:16:08 +0530

    Add specific quote load

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,16 +1,39 @@
+import { useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+import useHttp from "../hooks/use-http";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
 import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
-import { DUMMY_QUOTES } from "../App";
+import { getSingleQuote } from "../lib/api";
 
 export default function QuoteDetail() {
   const { quoteId } = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find(({ id }) => id === quoteId);
+  const {
+    status,
+    sendRequest,
+    data: quote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending")
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (error) return <p className="centered">{error}</p>;
+
+  if (status === "completed" && !quote.text) return <NoQuotesFound />;
 
   return (
     <>
