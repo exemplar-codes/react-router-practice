d155bd484dae61098168dd2a0659b1faba3e2c09
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-02 18:05:24 +0530

    POST http call

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,18 +1,32 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 export default function NewQuote() {
   const history = useHistory();
 
+  const { status, sendRequest } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === "completed") history.push("/quotes");
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
-    history.push("/quotes");
+
+    sendRequest(quoteData);
   };
 
   return (
     <>
       <h1>New Quote Page</h1>
-      <QuoteForm onAddQuote={addQuoteHandler} />
+      <QuoteForm
+        onAddQuote={addQuoteHandler}
+        isLoading={status === "pending"}
+      />
     </>
   );
 }
