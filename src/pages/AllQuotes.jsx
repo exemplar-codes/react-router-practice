0261254b0a5e4b032b9cf101fb6ac47891de557e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-02 18:08:19 +0530

    Add API call to load quotes

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,12 +1,42 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
 
-import { DUMMY_QUOTES } from "../App";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 export default function AllQuotes() {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending")
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0))
+    return <NoQuotesFound />;
+
   return (
     <>
       <h1>All Quotes Page</h1>
-      <QuoteList quotes={DUMMY_QUOTES} />
+      <QuoteList quotes={loadedQuotes} />
     </>
   );
 }
