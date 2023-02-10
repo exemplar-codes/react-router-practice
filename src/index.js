138fc8f4a6c58f5406ede39c238c09e1348fc76e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-10 16:21:48 +0530

    Use HashRouter for gh pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/react-router-practice/quotes">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
